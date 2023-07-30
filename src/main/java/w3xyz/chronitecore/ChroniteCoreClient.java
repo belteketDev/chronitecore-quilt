262fa9d459ab25c3bef7f692772cacc13ed0c871
belteketDev <103261807+belteketDev@users.noreply.github.com>
2023-07-30 11:24:57 +0300

    quilt setup done

---
+++ src/main/java/w3xyz/chronitecore/ChroniteCoreClient.java
@@ -0,0 +1,11 @@
+package w3xyz.chronitecore;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
+public class ChroniteCoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+
+	}
+}
